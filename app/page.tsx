95dc489e076f903f57d2e1834d0a04fd8046abc9
Alex Warda <alexsio03@gmail.com>
2025-01-16 13:09:30 -0800

    plaza changes on click, responsive navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,15 @@ import Navbar from "../components/navbar";
 
 export default function Home() {
   const [isDark, setIsDark] = useState(true);
+  const [activePlaza, setActivePlaza] = useState("Home");
 
   return (
     <div className={isDark ? "dark" : ""}>
       <div className="flex items-center justify-center relative min-h-screen bg-white text-gray-800 dark:bg-[#18181b] dark:text-gray-200 transition-colors">
         <Dots />
         <main className="z-10 border w-[60rem] h-[40rem] p-4 flex">
-          <Plaza />
-          <Navbar />
+          <Plaza activePlaza={activePlaza} />
+          <Navbar setActivePlaza={setActivePlaza} isDark={isDark} setIsDark={setIsDark} />
         </main>
       </div>
     </div>
--- components/plaza.tsx
+++ components/plaza.tsx
@@ -1,7 +1,7 @@
-export default function Plaza() {
+export default function Plaza({activePlaza}: {activePlaza: string}) {
   return (
     <div className="basis-3/4 bg-gray-600 h-full rounded-2xl p-4 mr-8">
-      <h1>Plaza</h1>
+      <h1>{activePlaza}</h1>
     </div>
   )
 }
