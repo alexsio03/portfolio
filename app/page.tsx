4cfed07fee86c15beea544bec2953aac92c278b6
Alex Warda <alexsio03@gmail.com>
2025-01-17 15:05:27 -0800

    neko and stars

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,62 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Dots from "../components/dots";
 import Plaza from "../components/plaza";
 import Navbar from "../components/navbar";
+import { Neko } from "neko-ts";
 
 export default function Home() {
-  const [isDark, setIsDark] = useState(true);
-  const [activePlaza, setActivePlaza] = useState("Home");
-  const [isTransitioning, setIsTransitioning] = useState(false);
+	const [isDark, setIsDark] = useState(true);
+	const [activePlaza, setActivePlaza] = useState("Home");
+	const [isTransitioning, setIsTransitioning] = useState(false);
 
-  const handlePlazaChange = (newPlaza: string) => {
-    setIsTransitioning(true);
-    setTimeout(() => {
-      setActivePlaza(newPlaza);
-      setIsTransitioning(false);
-    }, 500);
-  };
+	const neko = useRef<Neko>(null);
 
-  return (
-    <div className={isDark ? "dark" : ""}>
-      <div className="flex items-center justify-center relative min-h-screen bg-white text-gray-800 dark:bg-[#18181b] dark:text-gray-200 transition-colors">
-        <Dots />
-        <main className="z-10 w-[60rem] h-[40rem] p-4 flex">
-          <div
-            className={`flex-1 transition-opacity duration-500 ${
-              isTransitioning ? "opacity-0" : "opacity-100"
-            }`}
-          >
-            <Plaza activePlaza={activePlaza} />
-          </div>
-          <Navbar setActivePlaza={handlePlazaChange} isDark={isDark} setIsDark={setIsDark} />
-        </main>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		if (!neko.current) {
+			neko.current = new Neko({
+				origin: {
+					x: 100,
+					y: 100,
+				},
+				parent: document.getElementById("main") as HTMLElement,
+			});
+		}
+	}, [neko]);
+
+	const handlePlazaChange = (newPlaza: string) => {
+		setIsTransitioning(true);
+		setTimeout(() => {
+			setActivePlaza(newPlaza);
+			setIsTransitioning(false);
+		}, 500);
+	};
+
+	return (
+		<div className={isDark ? "dark" : ""}>
+			<div className="flex items-center justify-center relative min-h-screen bg-white text-gray-800 dark:bg-[#18181b] dark:text-gray-200 transition-colors">
+				<Dots />
+				<main
+					id="main"
+					className="z-10 w-full h-full flex items-center justify-center"
+				>
+					<div className="w-[60rem] h-[40rem] p-4 flex">
+						<div
+							className={`flex-1 transition-opacity duration-500 ${
+								isTransitioning ? "opacity-0" : "opacity-100"
+							}`}
+						>
+							<Plaza activePlaza={activePlaza} />
+						</div>
+						<Navbar
+							setActivePlaza={handlePlazaChange}
+							isDark={isDark}
+							setIsDark={setIsDark}
+						/>
+					</div>
+				</main>
+			</div>
+		</div>
+	);
 }
-
--- components/dots.tsx
+++ components/dots.tsx
@@ -1,10 +1,71 @@
-export default function Dots() {
-  return (
-    <div className="z-0">
-      <div className="absolute top-16 right-12 h-80 w-80 bg-[url('/dots.svg')] bg-repeat opacity-80 pointer-events-none" />
-      <div className="absolute top-32 right-28 h-80 w-80 bg-[url('/dots.svg')] bg-repeat opacity-90 pointer-events-none" />
-      <div className="absolute bottom-16 left-16 h-80 w-80 bg-[url('/dots.svg')] bg-repeat opacity-90 pointer-events-none" />
-      <div className="absolute bottom-40 left-40 h-80 w-80 bg-[url('/dots.svg')] bg-repeat opacity-80 pointer-events-none" />
-    </div>
-  );
-}
+import React, { useEffect, useMemo, useState, useCallback } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+	type Container,
+	type ISourceOptions,
+	MoveDirection,
+	OutMode,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
+const Dots = () => {
+	const [init, setInit] = useState(false);
+
+	useEffect(() => {
+		initParticlesEngine(async (engine) => {
+			await loadSlim(engine);
+		}).then(() => {
+			setInit(true);
+		});
+	}, []);
+
+	const particlesLoaded = useCallback(async (container?: Container): Promise<void> => {
+		console.log(container);
+	}, []);
+
+	const options: ISourceOptions = useMemo(
+		() => ({
+			background: {
+				color: "#18181b",
+			},
+			particles: {
+				number: {
+					value: 100,
+				},
+				move: {
+					direction: MoveDirection.none,
+					enable: true,
+					outModes: {
+						default: OutMode.out,
+					},
+					random: true,
+					speed: 0.1,
+					straight: false,
+				},
+				opacity: {
+					animation: {
+						enable: true,
+						speed: 1,
+						sync: false,
+					},
+					value: { min: 0, max: 1 },
+				},
+				size: {
+					value: { min: 1, max: 3 },
+				},
+			},
+		}),
+		[],
+	);
+
+	return init ? (
+		<Particles
+			id="tsparticles"
+			particlesLoaded={particlesLoaded}
+			options={options}
+		/>
+	) : null;
+};
+
+export default React.memo(Dots);
+
